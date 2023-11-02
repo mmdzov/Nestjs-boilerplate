0477fb81a137d5299a0cd04650c5218486248371
mmdzov <mzov939@gmail.com>
2023-11-02 16:47:22 +0330

    Delete: extra codes

--- src/main.ts
+++ src/main.ts
@@ -22,23 +22,6 @@ async function bootstrap() {
 
   await app.listen(http.port);
 
-  // const server = app.getHttpServer();
-  // const router = server._events.request._router;
-
-  // const availableRoutes: [] = router.stack
-  //   .map((layer) => {
-  //     if (layer.route) {
-  //       return {
-  //         route: {
-  //           path: layer.route?.path,
-  //           method: layer.route?.stack[0].method,
-  //         },
-  //       };
-  //     }
-  //   })
-  //   .filter((item) => item !== undefined);
-  // console.log(availableRoutes);
-
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
