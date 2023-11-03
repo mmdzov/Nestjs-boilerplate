92017a3d7f554a4d00007271e380e51082c19353
mmdzov <mzov939@gmail.com>
2023-11-03 14:01:25 +0330

    Add: zod schema

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-export class CreateUserDto {
-  username: string;
-  password: string;
-}
+import { createUserSchema } from './../schema/create.schema';
+import { z } from 'zod';
+
+export type createUserDto = z.infer<typeof createUserSchema>;
